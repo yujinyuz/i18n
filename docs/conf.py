65bc5fbc50b35acb63a42c26e3cba74f61d28c41
Yujin <jinyuzprodigy@gmail.com>
2017-09-27 22:37:41 +0800

    Add double period for path.

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 import os
 import sys
 import django
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'taskbuster.settings.production'
 django.setup()
 
