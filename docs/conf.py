5772934222dd5ada85961d9d741a01b3a9735528
Yujin <jinyuzprodigy@gmail.com>
2017-09-27 22:42:33 +0800

    Fix for sphinx build.

--- docs/conf.py
+++ docs/conf.py
@@ -19,10 +19,10 @@
 #
 import os
 import sys
-import django
+
 sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'taskbuster.settings.production'
-django.setup()
+from django.conf import settings
+settings.configure()
 
 # -- General configuration ------------------------------------------------
 
